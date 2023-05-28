e1f1855e8ce900b5e63e190628c0a10e697db86a
Erchamion <jon.staten@gmail.com>
2023-05-27 21:46:50 -0400

    add Property tests for NewSubscriber; refactor to use TryFrom

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -6,6 +6,17 @@ use uuid::Uuid;
 
 use crate::domain::{NewSubscriber, SubscriberName, SubscriberEmail};
 
+impl TryFrom<FormData> for NewSubscriber {
+	type Error = String;
+
+	fn try_from(value: FormData) -> Result<Self, Self::Error> {
+		let name = SubscriberName::parse(value.name)?;
+		let email = SubscriberEmail::parse(value.email)?;
+
+		Ok(Self { name, email })
+	}
+}
+
 #[derive(serde::Deserialize)]
 pub struct FormData {
 	name: String,
@@ -24,19 +35,9 @@ pub async fn subscribe(
 	form: web::Form<FormData>,
 	pool: web::Data<PgPool>,
 ) -> HttpResponse {
-	let name = match SubscriberName::parse(form.0.name) {
-		Ok(name) => name,
+	let new_subscriber = match form.0.try_into() {
+		Ok(form) => form,
 		Err(_) => return HttpResponse::BadRequest().finish(),
-	};
-
-	let email = match SubscriberEmail::parse(form.0.email) {
-		Ok(email) => email,
-		Err(_) => return HttpResponse::BadRequest().finish(),
-	};
-	
-	let new_subscriber = NewSubscriber {
-		email,
-		name,
 	};
 
 	match insert_subscriber(&pool, &new_subscriber).await
