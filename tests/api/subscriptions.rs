2553565344fd259d1073a068da9e707f41ce6c1e
staten <jon.staten@leadingagile.com>
2023-06-14 03:01:15 -0400

    test: add helper post_subscription

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -3,16 +3,9 @@ use crate::helpers;
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let test_app = helpers::spawn_app().await;
-    let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
-    let response = client
-        .post(&format!("{}/subscriptions", &test_app.address))
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .body(body)
-        .send()
-        .await
-        .expect("Failed to execute request");
+    let response = test_app.post_subscription(body.into()).await;
 
     assert_eq!(200, response.status().as_u16());
 
@@ -29,7 +22,6 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
     // ARRANGE
     let app = helpers::spawn_app().await;
-    let client = reqwest::Client::new();
     let test_cases = vec![
         ("name=&email=ursula_le_quin%40gmail.com", "empty name"),
         ("name=Ursula&email=", "empty email"),
@@ -38,13 +30,7 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
 
     for (body, description) in test_cases {
         // ACT
-        let response = client
-            .post(&format!("{}/subscriptions", &app.address))
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .body(body)
-            .send()
-            .await
-            .expect("Failed to execute request.");
+        let response = app.post_subscription(body.into()).await;
 
         assert_eq!(
             400,
@@ -58,7 +44,6 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
 #[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
     let test_app = helpers::spawn_app().await;
-    let client = reqwest::Client::new();
     let test_cases = vec![
         ("name=le%20guin", "missing email"),
         ("email=ursurla_le_%40gmail.com", "missing name"),
@@ -66,13 +51,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     ];
 
     for (invalid_body, error_message) in test_cases {
-        let response = client
-            .post(&format!("{}/subscriptions", &test_app.address))
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .body(invalid_body)
-            .send()
-            .await
-            .expect("Failed to execute request");
+        let response = test_app.post_subscription(invalid_body.into()).await;
 
         assert_eq!(
             400,
