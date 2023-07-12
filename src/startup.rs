3850eab4d7b7925dd7de064e8d3c18f2b2540974
staten <jon.staten@leadingagile.com>
2023-07-12 02:09:38 -0400

    refactor: secure query params using hmac

--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,7 @@ use std::net::TcpListener;
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use secrecy::Secret;
 use sqlx::postgres::PgPoolOptions;
 use tracing_actix_web::TracingLogger;
 
@@ -12,6 +13,9 @@ use crate::email_client::EmailClient;
 use crate::routes::{
     confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
 };
+
+#[derive(Clone)]
+pub struct HmacSecret(pub Secret<String>);
 
 pub struct Application {
     port: u16,
@@ -50,6 +54,7 @@ impl Application {
             connection_pool,
             email_client,
             configuration.application.base_url,
+            configuration.application.hmac_secret,
         )?;
 
         Ok(Self { port, server })
@@ -77,6 +82,7 @@ pub fn run(
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
+    hmac_secret: Secret<String>,
 ) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
     let email_client = web::Data::new(email_client);
@@ -94,6 +100,7 @@ pub fn run(
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
+            .app_data(web::Data::new(HmacSecret(hmac_secret.clone())))
     })
     .listen(listener)?
     .run();
