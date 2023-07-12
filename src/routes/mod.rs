c9217610a7bdb54bf39545bf4532d90a34ac46a9
staten <jon.staten@leadingagile.com>
2023-07-11 23:52:05 -0400

    feat: add home route

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,9 +1,11 @@
 mod health_check;
+mod home;
 mod newsletter;
 mod subscription_confirm;
 mod subscriptions;
 
 pub use health_check::*;
+pub use home::*;
 pub use newsletter::*;
 pub use subscription_confirm::*;
 pub use subscriptions::*;
