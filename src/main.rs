340af96b70fe5d9db4b4a3f5dade4dd62f7e42c5
Jon Staten <jon.staten@gmail.com>
2023-11-18 09:31:42 -0500

    refactor: add redis session

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use zerocool::{
 };
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> anyhow::Result<()> {
     let subscriber = get_subscriber("zerocool".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
